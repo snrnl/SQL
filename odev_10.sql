8b4ccdbe0c50017ddd073c35344955b0a2cc328b
Soner Ünal <104282439+snrnl@users.noreply.github.com>
2022-12-11 18:13:59 +0300

    Add files via upload

---
+++ odev_10.sql
@@ -0,0 +1,15 @@
+--1
+
+SELECT city, country FROM city 
+LEFT JOIN country ON city.country_id = country.country_id;
+
+--2
+
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
+RIGHT JOIN customer ON payment.customer_id = customer.customer_id ;
+
+--3
+
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental
+FULL JOIN customer ON rental.customer_id = customer.customer_id ;
+
